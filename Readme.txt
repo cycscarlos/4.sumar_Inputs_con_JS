62dac25bc8081a8e3c0ef2d70ce859b4e7291730
cycscarlos <cycs.ingenieria@gmail.com>
2021-12-09 15:48:17 -0400

    first commit

---
+++ Readme.txt
@@ -0,0 +1,23 @@
+
+2 de Noviembre del 2021.
+
+Javascript Learning. Seriado #4.                   
+
+Contenido: "SUMAR números aleatorio, comprobar y emitir resultados con Javascript"
+------------------
+
+1. Se crearan tres INPUT, dos en modo READONLY, para cargar allí números aleatorios
+producidos por la función Math.random()
+2. El otro INPUT es para que el usuario coloque el resultado de la suma de los dos númeroas aleatorios
+3. Se valiarán las sumas y en un DIV aparte se colocarán los resultados y un mensaje de si la suma fue
+correcta o no
+4. Así mismo, se colocará color rojo al texto, si la respuesta fue incorrecta o verde si fue correcta
+5. Se crearán funciones para no repetir codigo, y las mismas se ejecutarán en la función de INICIO del
+la app Javascript.
+6. Se contruyeron dos APPs que hacen los mismo pero de diferente manera!
+
+
+
+
+	
+ 
---
+++ assets/js/app_v2.js
@@ -0,0 +1,52 @@
+window.onload = inicio;
+let a, b, sumaCorrecta;
+
+function inicio() {
+  preguntar();
+  document.querySelector(".boton").onclick = comprobar;
+  document.querySelector(".tres").onkeydown = teclado;
+}
+
+function preguntar() {
+  // Se escogen dos números aleatorios, entre 0 y 9, y se le eliminan los decimales y su valor se almacena en dos variables de ambito global
+  a = Math.floor(Math.random() * 10);
+  b = Math.floor(Math.random() * 10);
+
+  // Se suman los números aleatorios para después validar con el resultado ingresado en el input3
+  sumaCorrecta = a + b;
+
+  // Se almacenan los números aleatorios en los input1 e input2 respectivamente, para mostrarlos por pantalla
+  document.querySelector(".uno").value = a;
+  document.querySelector(".dos").value = b;
+
+  // Se limpia el área del input3 y se coloca el cursor dentro para facilitar la escritura
+  document.querySelector(".tres").value = "";
+  document.querySelector(".tres").focus();
+}
+
+function teclado(e) {
+  let tecla = e.key;
+  if (tecla == "enter") {
+    comprobar();
+  }
+}
+
+function comprobar() {
+  let c = Number(document.querySelector(".tres").value);
+  if (c == sumaCorrecta) {
+    document
+      .querySelector(".resultados")
+      .insertAdjacentHTML(
+        "beforeend",
+        "<div class='verde'>¡Es correcto!</div>"
+      );
+  } else {
+    document
+      .querySelector(".resultados")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div class="rojo">¡Es incorrecto! El resultado de sumar ${a} + ${b} = ${sumaCorrecta}</div>`
+      );
+  }
+  preguntar();
+}
